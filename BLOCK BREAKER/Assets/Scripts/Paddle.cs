9269926e1b23d1dd38f1c9b1c31fc23a67bea58b
rahul72p <rahul72p.rp@gmail.com>
2020-03-29 21:31:33 +0530

    first commit

---
+++ BLOCK BREAKER/Assets/Scripts/Paddle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	public float paddleSpeed = 3f;
+
+
+	private Vector3 playerPos = new Vector3 (0, 0, 1f);
+
+	void Update () 
+	{
+		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+		playerPos = new Vector3 (Mathf.Clamp (xPos, -7f, 7f), 0f, 1f);
+		transform.position = playerPos;
+
+	}
+}
